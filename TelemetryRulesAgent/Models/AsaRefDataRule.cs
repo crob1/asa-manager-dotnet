c572e4193280f2178b8ce8b694025baccc16567f
crob1 <charlesrobin@outlook.com>
2018-08-09 09:27:51 +0200

    Update AsaRefDataRule.cs
    Fix the operatorsMap where Equals is mapped to = but should be == to work in ASA UDF.

--- TelemetryRulesAgent/Models/AsaRefDataRule.cs
+++ TelemetryRulesAgent/Models/AsaRefDataRule.cs
@@ -77,7 +77,7 @@ namespace Microsoft.Azure.IoTSolutions.AsaManager.TelemetryRulesAgent.Models
                 { "<", "<" },
                 { "LessThanOrEqual", "<=" },
                 { "<=", "<=" },
-                { "Equals", "=" },
+                { "Equals", "==" },
                 { "=", "=" },
                 { "==", "=" },
             };
